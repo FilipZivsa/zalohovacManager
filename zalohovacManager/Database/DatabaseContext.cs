bcb21d4b2aabe9e2b29b37222e399da98882999a
Filip Živsa <filip.zivsa@gmail.com>
2026-06-08 21:20:24 +0200

    added packages and made files

---
+++ zalohovacManager/Database/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace zalohovacManager.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        //public DbSet<Car> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+            optionsBuilder.UseMySQL("server=mysqlstudenti.litv.sssvt.cz;database=3a1_zivsafilip_db1;user=zivsafilip;password=654321;charset=utf8;");
+        }
+    }
+}
