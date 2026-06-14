1d880c742998cbb393c6e655d00482cec4e804a3
Filip Živsa <filip.zivsa@gmail.com>
2026-06-14 08:45:09 +0200

    added JWT

--- zalohovacManager/Program.cs
+++ zalohovacManager/Program.cs
@@ -2,6 +2,9 @@
 using Microsoft.EntityFrameworkCore;
 using zalohovacManager.Database;
 using zalohovacManager.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace zalohovacManager
 {
@@ -29,7 +32,23 @@ namespace zalohovacManager
 
 
 
-
+            // jwt config
+            var jwtKey = builder.Configuration["Jwt:Key"];
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+                    };
+                });
+          
 
             // Add services to the container.
 
@@ -47,6 +66,7 @@ namespace zalohovacManager
                 app.UseSwaggerUI();
             }
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
